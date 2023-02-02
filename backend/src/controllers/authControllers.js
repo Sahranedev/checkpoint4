4f9daa9d8ea7e172d4b2cc57ff8122443bcf8081
Sahrane_Guassemi <sahranedev@gmail.com>
2023-02-02 14:09:42 +0100

    save fix

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -2,12 +2,12 @@ const models = require("../models");
 
 const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
   const { email } = req.body;
-  console.log("-- enter in authCon");
-  console.log("Mail ", email);
+  console.warn("-- enter in authCon");
+  console.warn("Mail ", email);
   models.user
     .findByEmailWithPassword(email)
     .then(([users]) => {
-      console.log(users);
+      console.warn(users);
       if (users[0]) {
         [req.user] = users;
         next();
@@ -21,12 +21,12 @@ const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
 
 const getProfByEmailWithPasswordAndPassToNext = (req, res, next) => {
   const { email } = req.body;
-  console.log("-- enter in authCon");
-  console.log("Mail ", email);
+  console.warn("-- enter in authCon");
+  console.warn("Mail ", email);
   models.professor
     .findByEmailWithPassword(email)
     .then(([professor]) => {
-      console.log(professor);
+      console.warn(professor);
       if (professor[0]) {
         [req.professor] = professor;
         next();
